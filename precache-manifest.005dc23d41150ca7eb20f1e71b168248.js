e68ef665eea4a2edfd26a92c9322696153591779
Steve <steve.maina@student.moringaschool.com>
2022-10-07 11:14:43 +0300

    Updates

---
+++ precache-manifest.005dc23d41150ca7eb20f1e71b168248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbe53cfc740a97af6b0c",
+    "url": "/Photo-likes/static/css/main.2f1c7ae3.chunk.css"
+  },
+  {
+    "revision": "fbe53cfc740a97af6b0c",
+    "url": "/Photo-likes/static/js/main.8aa9e577.chunk.js"
+  },
+  {
+    "revision": "82b7adee1001a4c6ad04",
+    "url": "/Photo-likes/static/js/runtime~main.34503893.js"
+  },
+  {
+    "revision": "d8dde379715146c57a38",
+    "url": "/Photo-likes/static/js/2.003fd567.chunk.js"
+  },
+  {
+    "revision": "130037627cfdb1204a9c360cf637e3a5",
+    "url": "/Photo-likes/index.html"
+  }
+];
